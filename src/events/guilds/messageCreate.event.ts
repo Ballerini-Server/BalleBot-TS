f49c03609e9370dd21c80407fe2f745858a2563d
tauz-hub <tauasanto101@gmail.com>
2022-07-23 00:28:45 -0300

    :rocket: function to check if command has prefix

--- src/events/guilds/messageCreate.event.ts
+++ src/events/guilds/messageCreate.event.ts
@@ -2,16 +2,24 @@ import "dotenv/config";
 import { Message } from "eris";
 import { Ballebot } from "../../structures/Client";
 import { EventBase } from "../../structures/Event";
+import { startWithPrefix } from "../../utils/events/messageCreate/startWithPrefix";
 
-export default new EventBase("messageCreate", (message: Message) => {
-  if (!message.content.startsWith(process.env.PREFIX)) return;
+export default new EventBase("messageCreate", async (message: Message) => {
+  if (message.author.bot) return;
+  if (message.content === "") return;
+  if (!(await startWithPrefix(message))) return;
 
   const args = message.content.slice(1).split(/ +/);
+  const commandName = args.shift().toLowerCase();
 
   const ballebot = Ballebot.getInstance();
-  const command = ballebot
+  const commandToRun = ballebot
     .getListCommands()
-    .find((command) => command.name === args[0]);
-  if (!command) return;
-  command.run(message);
+    .find(
+      (command) =>
+        command.name === commandName || command.aliases?.includes(commandName)
+    );
+
+  if (!commandToRun) return;
+  commandToRun.run(message);
 });
